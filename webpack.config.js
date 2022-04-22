63f30fc1b3eb43d7467b49c93cf5046abbbeb938
Andreas <andreas95560@gmail.com>
2022-04-22 15:07:19 +0200

    Config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  entry: './js/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+}
